1829fa05d9381192ab432fd02bafb65f315a24ab
Thomas Montague <montague.thomas@lmbbox.com>
2013-03-18 22:17:55 -0400

    Removed app.conf and app.php from history and reimported.

---
+++ application/config/app.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$config['app_bin_root']			= APPPATH . '/bin';
+$config['app_sites_root']		= '/var/www';
+$config['app_sites_template']	= 'example.com';
+$config['app_sites_fields']		= array(
+	'suffix' => array(
+		".{$_SERVER['SERVER_ADDR']}.xip.io" => array(
+			'label'		=> ".{$_SERVER['SERVER_ADDR']}.xip.io",
+			'value'		=> ".{$_SERVER['SERVER_ADDR']}.xip.io",
+		),
+		'.xip.io' => array(
+			'label'		=> '.xip.io',
+			'value'		=> '.xip.io',
+		),
+		'.vcap.me' => array(
+			'label'		=> '.vcap.me',
+			'value'		=> '.vcap.me',
+		),
+		'.dev' => array(
+			'label'		=> '.dev',
+			'value'		=> '.dev',
+		),
+		'.local' => array(
+			'label'		=> '.local',
+			'value'		=> '.local',
+		),
+	),
+	'template' => array(
+		'' => array(
+			'label'		=> 'None',
+			'value'		=> '',
+		),
+		'drupal7' => array(
+			'label'		=> 'Drupal 7',
+			'value'		=> 'drupal7',
+			'script'	=> 'drush-restore.sh',
+			'source'	=> 'http://example.com/drupal7.tar.gz',
+		),
+	),
+);
+
+
+/* End of file app.php */
+/* Location: ./application/config/app.php */
