28ec3bed97e442ec4f7e0b988686ff669afdd5c1
Thomas Montague <montague.thomas@lmbbox.com>
2013-05-10 16:13:27 -0400

    Updated configs and documentation to use /usr/local/share instead of /usr/share for installation location.

--- application/bin/install.sh
+++ application/bin/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## Clone repo to install location.
-git clone --recursive git://github.com/lmbbox/lampp.git /usr/share/lampp
+git clone --recursive git://github.com/lmbbox/lampp.git /usr/local/share/lampp
 
 ## Copy sudoers file.
-cp /usr/share/lampp/application/config/sudoers /etc/sudoers.d/lampp
+cp /usr/local/share/lampp/application/config/sudoers /etc/sudoers.d/lampp
 
 ## Added Apache site config, enable, and restart.
-ln -s /usr/share/lampp/application/config/apache.conf /etc/apache2/sites-available/lampp
+ln -s /usr/local/share/lampp/application/config/apache.conf /etc/apache2/sites-available/lampp
 a2ensite lampp
 service apache2 restart
