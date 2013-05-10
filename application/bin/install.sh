7a054f632dacce6c2d6f81813e39e03a9af0c1ad
Thomas Montague <montague.thomas@lmbbox.com>
2013-05-10 17:18:10 -0400

    Added lines to set correct folder/file permissions to install.sh script. Added execute permission to shell scripts.

--- application/bin/install.sh
+++ application/bin/install.sh
@@ -3,6 +3,10 @@
 ## Clone repo to install location.
 git clone --recursive git://github.com/lmbbox/lampp.git /usr/local/share/lampp
 
+## Set correct folder/file permissions
+chmod 777 /usr/local/share/lampp/application/cache
+chmod 777 /usr/local/share/lampp/application/logs
+
 ## Copy sudoers file.
 cp /usr/local/share/lampp/application/config/sudoers /etc/sudoers.d/lampp
 
