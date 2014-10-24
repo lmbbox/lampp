7242ff4def32e9d050b36ccd0f23370049d4817b
Thomas Montague <montague.thomas@lmbbox.com>
2014-10-24 17:35:39 -0400

    Updated install script to setup new system user.

--- application/bin/install.sh
+++ application/bin/install.sh
@@ -7,7 +7,7 @@ $LAMPP_ROOT="/usr/local/share/lampp"
 
 
 ## Clone repo to install location.
-git clone --recursive git://github.com/lmbbox/lampp.git "$LAMPP_ROOT"
+git clone --recursive "git://github.com/lmbbox/lampp.git" "$LAMPP_ROOT"
 
 
 ## Set correct folder/file permissions
@@ -16,10 +16,15 @@ chmod 777 "$LAMPP_ROOT/application/logs"
 
 
 ## Copy sudoers file.
-cp "$LAMPP_ROOT/application/config/sudoers" /etc/sudoers.d/lampp
+cp "$LAMPP_ROOT/application/config/sudoers" "/etc/sudoers.d/lampp"
+
+
+# Setup new system user/group and set ownership
+sudo useradd --system --home-dir "$LAMPP_ROOT" --shell /bin/false lampp
+chown -R lampp:lampp "$LAMPP_ROOT"
 
 
 ## Added Apache site config, enable, and restart.
-ln -s "$LAMPP_ROOT/application/config/apache.conf" /etc/apache2/sites-available/lampp
-a2ensite lampp
+ln -s "$LAMPP_ROOT/application/config/apache.conf" "/etc/apache2/sites-available/lampp.conf"
+ln -s "../sites-available/lampp.conf" "/etc/apache2/sites-enabled/000-lampp.conf"
 service apache2 restart
